c3aa42ad235f3c7749d63d8a37b9280271c61684
Roman Kostin <36431458+romanvht@users.noreply.github.com>
2024-03-25 14:28:14 +1000

    Update gigachat.php

--- gigachat.php
+++ gigachat.php
@@ -1,40 +1,34 @@
 <?php
-namespace gigachat;
+namespace neiro;
 
-class Gigachat
-{
-  protected const CLIENT_AUTH = "<AUTH CODE>";
+class Gigachat {
+  protected const CLIENT_AUTH = "key";
 
   protected static $instance;
   private static $token = false;
   private static $tokenExp = false;
   private static $messages = [];
 
-  public static function getInstance()
-  {
+  public static function getInstance() {
     if (is_null(self::$instance)) {
-      self::$instance = new self;
+      self::$instance = new self();
       self::$token = file_get_contents('token.txt');
       self::$tokenExp = file_get_contents('token_ext.txt');
     }
     return self::$instance;
   }
 
-  public static function getHistory()
-  {
+  public static function getHistory() {
     return self::$messages;
   }
-  public static function clearHistory()
-  {
+  public static function clearHistory() {
     self::$messages = [];
   }
-  public static function updateHistory($messages)
-  {
+  public static function updateHistory($messages) {
     self::$messages = $messages;
   }
 
-  private static function get($url, $headers, $data)
-  {
+  private static function get($url, $headers, $data) {
     $curl = curl_init();
     curl_setopt_array($curl, [
       CURLOPT_URL => $url,
@@ -47,17 +41,18 @@ class Gigachat
       curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
     }
 
-
     $result = curl_exec($curl);
     return json_decode($result, true);
   }
 
-  private static function get_image($token, $image)
-  {
+  private static function get_image($token, $image) {
     $curl = curl_init();
 
-    curl_setopt_array($curl, array(
-      CURLOPT_URL => 'https://gigachat.devices.sberbank.ru/api/v1/files/' . $image . '/content',
+    curl_setopt_array($curl, [
+      CURLOPT_URL =>
+        'https://gigachat.devices.sberbank.ru/api/v1/files/' .
+        $image .
+        '/content',
       CURLOPT_SSL_VERIFYPEER => 0,
       CURLOPT_RETURNTRANSFER => true,
       CURLOPT_ENCODING => '',
@@ -66,12 +61,11 @@ class Gigachat
       CURLOPT_FOLLOWLOCATION => true,
       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
       CURLOPT_CUSTOMREQUEST => 'GET',
-      CURLOPT_HTTPHEADER => array(
+      CURLOPT_HTTPHEADER => [
         'Accept: application/jpg',
-        'Authorization: Bearer ' . $token
-      ),
-    )
-    );
+        'Authorization: Bearer ' . $token,
+      ],
+    ]);
 
     $result = curl_exec($curl);
 
@@ -85,28 +79,31 @@ class Gigachat
     return $img;
   }
 
-  private static function guidv4($data = null)
-  {
+  private static function guidv4($data = null) {
     $data = $data ?? random_bytes(16);
     assert(strlen($data) == 16);
 
-    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
-    $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+    $data[6] = chr((ord($data[6]) & 0x0f) | 0x40);
+    $data[8] = chr((ord($data[8]) & 0x3f) | 0x80);
 
     return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
   }
 
-  public static function getToken($force = false)
-  {
-    if (!self::$token || !self::$tokenExp || self::$tokenExp < time() || $force === true) {
+  public static function getToken($force = false) {
+    if (
+      !self::$token ||
+      !self::$tokenExp ||
+      self::$tokenExp < time() ||
+      $force === true
+    ) {
       $url = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth";
       $headers = [
         'Authorization: Bearer ' . self::CLIENT_AUTH,
         'RqUID: ' . self::guidv4(),
-        'Content-Type: application/x-www-form-urlencoded'
+        'Content-Type: application/x-www-form-urlencoded',
       ];
       $data = [
-        'scope' => 'GIGACHAT_API_PERS'
+        'scope' => 'GIGACHAT_API_PERS',
       ];
       $result = self::get($url, $headers, http_build_query($data));
 
@@ -125,8 +122,7 @@ class Gigachat
     return self::$token;
   }
 
-  public static function answer($question, $temperature = 0.7)
-  {
+  public static function answer($question, $temperature = 0.7) {
     $answer = "";
     if (!empty($question)) {
       $tok = self::getToken();
@@ -142,20 +138,20 @@ class Gigachat
         $headers = [
           'Authorization: Bearer ' . $tok,
           'Content-Type: application/json',
-          'Accept: application/json'
+          'Accept: application/json',
         ];
         if ($no_history === false) {
           $messages = self::$messages;
         }
         $messages[] = [
           "role" => "user",
-          "content" => $question
+          "content" => $question,
         ];
         $data = [
           "model" => "GigaChat:latest",
           "temperature" => $temperature,
           "max_tokens" => 1024,
-          "messages" => $messages
+          "messages" => $messages,
         ];
         $result = self::get($url, $headers, json_encode($data));
 
@@ -165,17 +161,21 @@ class Gigachat
           if ($no_history === false) {
             $messages[] = [
               "role" => "assistant",
-              "content" => $answer
+              "content" => $answer,
             ];
 
             self::$messages = $messages;
+          }
+
+          if (count($messages) > 1000) {
+            self::clearHistory();
           }
 
           preg_match_all('/<img[^>]*?src=\"(.*)\"/iU', $answer, $imageSearch);
           if (isset($imageSearch[1][0])) {
             $answer = preg_replace('/<img(?:\\s[^<>]*)?>/i', '', $answer);
             $image = self::get_image($tok, $imageSearch[1][0]);
-            $answer .= 'http://' . $_SERVER['SERVER_ADDR'] . '/' . $image;
+            $answer .= 'http://82.147.71.126/' . $image;
           }
         }
       }
